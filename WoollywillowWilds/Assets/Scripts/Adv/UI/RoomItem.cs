03a5dbf6aae0c38c9eab58fd06e77128474213e2
mysterymagination <creswel2@gmail.com>
2025-12-08 22:46:38 -0500

    issue #4: adding prefabs for garden room and item detail

--- WoollywillowWilds/Assets/Scripts/Adv/UI/RoomItem.cs
+++ WoollywillowWilds/Assets/Scripts/Adv/UI/RoomItem.cs
@@ -5,13 +5,20 @@ namespace WildsAdv
 {
     public class RoomInteraction : MonoBehaviour, IPointerClickHandler
     {
-        public string ObjectID;
+        /**
+         * Human readable object name assigned in the editor.
+         */
+        public string objectID;
+        /**
+         * A GameObject prefab with a UI panel and image view ready to host the <objectId>_DetailedImage sprite.
+         */
+        public GameObject detailPanelPrefab;
 
         // Detect if a click occurs
         public void OnPointerClick(PointerEventData pointerEventData)
         {
             // Output to console the clicked GameObject's name and the following message.
-            Debug.Log(ObjectID + " Game Object Clicked at " + pointerEventData);
+            Debug.Log(objectID + " Game Object Clicked at " + pointerEventData);
         }
     }
 }
