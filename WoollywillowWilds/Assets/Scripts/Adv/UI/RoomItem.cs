9bf039a50760023ac63c5efcd2e77fff38474066
Jeff Creswell <creswel2@gmail.com>
2025-12-12 18:12:26 -0500

    laying out todos for item detail image

--- WoollywillowWilds/Assets/Scripts/Adv/UI/RoomItem.cs
+++ WoollywillowWilds/Assets/Scripts/Adv/UI/RoomItem.cs
@@ -11,6 +11,7 @@ namespace WildsAdv
         public string objectID;
         /**
          * A GameObject prefab with a UI panel and image view ready to host the <objectId>_DetailedImage sprite.
+         * TODO: is this the right way to instantiate a given prefab? I thought GameObjects had to be active in a scene? It seems like an asset path string or similar would make more sense.
          */
         public GameObject detailPanelPrefab;
 
@@ -19,6 +20,9 @@ namespace WildsAdv
         {
             // Output to console the clicked GameObject's name and the following message.
             Debug.Log(objectID + " Game Object Clicked at " + pointerEventData);
+
+            // TODO: load the ${objectID}_ItemDetail image into the detailPanelPrefab's ItemPanel -> ItemImage imageview. 
+            // TODO: load the GenerateItemDescription() result into StoryText. 
         }
     }
 }
