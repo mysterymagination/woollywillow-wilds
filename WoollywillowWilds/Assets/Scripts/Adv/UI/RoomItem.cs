281516742a94324dc05cf02f91a87d92f1084db6
mysterymagination <creswel2@gmail.com>
2025-12-15 23:13:47 -0500

    working on item detail panel popup

--- WoollywillowWilds/Assets/Scripts/Adv/UI/RoomItem.cs
+++ WoollywillowWilds/Assets/Scripts/Adv/UI/RoomItem.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace WildsAdv
 {
@@ -10,10 +11,13 @@ namespace WildsAdv
          */
         public string objectID;
         /**
-         * A GameObject prefab with a UI panel and image view ready to host the <objectId>_DetailedImage sprite.
-         * TODO: is this the right way to instantiate a given prefab? I thought GameObjects had to be active in a scene? It seems like an asset path string or similar would make more sense.
+         * A GameObject prefab with a UI panel and image view ready to host the detail image.
          */
         public GameObject detailPanelPrefab;
+        /**
+         * A GameObject with texture data representing the item detail image.
+         */
+        public Sprite detailImage;
 
         // Detect if a click occurs
         public void OnPointerClick(PointerEventData pointerEventData)
@@ -21,7 +25,20 @@ namespace WildsAdv
             // Output to console the clicked GameObject's name and the following message.
             Debug.Log(objectID + " Game Object Clicked at " + pointerEventData);
 
-            // TODO: load the ${objectID}_ItemDetail image into the detailPanelPrefab's ItemPanel -> ItemImage imageview. 
+            GameObject roomView = GameObject.FindWithTag("RoomView");
+            // load the ${objectID}_ItemDetail image into the detailPanelPrefab's ItemPanel -> ItemImage imageview. 
+            if (roomView && detailPanelPrefab)
+            {
+                // Add image texture2d to item detail panel.
+                // TODO: why is the instantiated subcanvas not rendering over the main canvas?
+                GameObject detailPanel = Instantiate(detailPanelPrefab, roomView.transform.position, roomView.transform.rotation, roomView.transform);//new Vector3(0.0F, 0.0F, 0.0F), Quaternion.identity);
+                /*
+                GameObject itemImage = detailPanel.transform.Find("ItemPanel").Find("ItemImage").gameObject;
+                Image image = itemImage.GetComponent<Image>();
+                image.sprite = detailImage;
+                */
+
+            }
             // TODO: load the GenerateItemDescription() result into StoryText. 
         }
     }
